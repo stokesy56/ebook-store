e79bcd66d4b944e23cdd7b65e55fd7dd765117cb
Rory Stokes <rstokes@spartaglobal.com>
2019-10-02 23:21:45 +0100

    initial commit with sql databse and table of books created

---
+++ SQL stuff.sql
@@ -0,0 +1,20 @@
+--CREATE DATABASE books_db
+
+--CREATE TABLE Books
+--(
+--BookID int NOT NULL,
+--Title VARCHAR(50),
+--Author VARCHAR(30),
+--ReleaseDate DATE
+--)
+
+--INSERT INTO Books
+--VALUES 
+--(1, 'Stormbreaker', 'Anthony Horowitz', '2000-09-04'),
+--(2, 'Fight Club', 'Chuck palahniuk', '1996-06-17'),
+--(3, 'The Time Machine', 'H. G. Wells', '1895-01-01'),
+--(4, 'Lord Loss', 'Darren Shan', '2005-05-12'),
+--(5, 'Snow Crash', 'Neal Stephenson', '1992-05-01')
+
+
+
